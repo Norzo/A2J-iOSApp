95734c0f7cf51dad48094f5b3abedd2a08af6ec7
Markus Andhoey <psyma13@sulu.local>
2021-03-11 16:08:13 +0000

    Initial creation of Xcode project

---
+++ A2J_iOSApp/A2J_iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A2J_iOSApp
+//
+//  Created by Markus Andhoey on 11/03/2021.
+//  Copyright © 2021 A2J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
