29a37a55bf07202bdb9ce73e70638c5fa05cd9a0
tkk7406 <56356311+tkk7406@users.noreply.github.com>
2021-03-18 15:38:04 +0000

    Finished main layout

---
+++ A2J_iOSApp/A2J_iOSApp/MovingInVC.swift
@@ -0,0 +1,19 @@
+//
+//  MovingInVC.swivt
+//  A2J_iOSApp
+//
+//  Created by Aristotelis Mitsiou on 18/03/2021.
+//  Copyright © 2021 A2J. All rights reserved.
+//
+
+import UIKit
+
+class MovingInVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
